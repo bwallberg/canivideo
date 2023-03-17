fa8d8c208819209e840e1a778268de940f5a18cb
Benjamin Wallberg <benjamin.wallberg@eyevinn.se>
2023-03-17 20:41:58 +0100

    feat(#2): test webm & hls as containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { For } from "solid-js";
 import { styled } from "solid-styled-components";
 import Codec from "./components/Codec";
 
@@ -51,6 +52,16 @@ const Footer = styled("footer")`
   }
 `;
 
+type ContainerType = "video/mp4" | "video/webm" | "application/vnd.apple.mpegurl";
+
+const ContainerTitle: { [key in ContainerType]: string} = {
+  "video/mp4": "MP4",
+  "video/webm": "WEBM",
+  "application/vnd.apple.mpegurl": "HTTP Live Streaming (HLS)"
+}
+
+const VIDEO_CONTAINER: ContainerType[] = ["video/mp4", "video/webm", "application/vnd.apple.mpegurl"];
+
 function App() {
   return (
     <Wrapper>
@@ -58,39 +69,49 @@ function App() {
         <h1>CAN I VIDEO?</h1>
       </Header>
       <Container>
+        <For each={VIDEO_CONTAINER}>
+          {(videoContainer) => (
+            <GridWrapper>
+              <h2>VIDEO - {ContainerTitle[videoContainer]}</h2>
+              <Grid>
+                <Codec
+                  title="Advanced Video Coding, h.264"
+                  contentType={`${videoContainer}; codecs="avc1.42E01E"`}
+                />
+                <Codec
+                  title="High Efficiency Video Coding, h.265/HEVC | hvc1"
+                  contentType={`${videoContainer}; codecs="hvc1.1.6.L93.90"`}
+                />
+                <Codec
+                  title="High Efficiency Video Coding, h.265/HEVC | hev1"
+                  contentType={`${videoContainer}; codecs="hev1.1.6.L93.90"`}
+                />
+                <Codec
+                  title="AOMedia Video 1, AV1 | 8 bits"
+                  contentType={`${videoContainer}; codecs="av01.0.00M.08"`}
+                />
+                <Codec
+                  title="AOMedia Video 1, AV1 | 10 bits"
+                  contentType={`${videoContainer}; codecs="av01.0.00M.10"`}
+                />
+                <Codec
+                  title="AOMedia Video 1, AV1 | 12 bits"
+                  contentType={`${videoContainer}; codecs="av01.0.00M.12"`}
+                />
+                <Codec
+                  title="Dolby Vision, dvhe"
+                  contentType={`${videoContainer}; codecs="dvhe.05.07"`}
+                />
+                <Codec
+                  title="Dolby Vision, dvh1"
+                  contentType={`${videoContainer}; codecs="dvh1.05.07"`}
+                />
+              </Grid>
+            </GridWrapper>
+          )}
+        </For>
         <GridWrapper>
-          <h2>VIDEO CODECS - video/mp4</h2>
-          <Grid>
-            <Codec
-              title="Advanced Video Coding, h.264"
-              contentType='video/mp4; codecs="avc1.42E01E"'
-            />
-            <Codec
-              title="High Efficiency Video Coding, h.265/HEVC | hvc1"
-              contentType='video/mp4; codecs="hvc1.1.6.L93.90"'
-            />
-            <Codec
-              title="High Efficiency Video Coding, h.265/HEVC | hev1"
-              contentType='video/mp4; codecs="hev1.1.6.L93.90"'
-            />
-            <Codec
-              title="AOMedia Video 1, AV1 | 8 bits"
-              contentType='video/mp4; codecs="av01.0.00M.08"'
-            />
-            <Codec
-              title="AOMedia Video 1, AV1 | 10 bits"
-              contentType='video/mp4; codecs="av01.0.00M.10"'
-            />
-            <Codec
-              title="AOMedia Video 1, AV1 | 12 bits"
-              contentType='video/mp4; codecs="av01.0.00M.12"'
-            />
-            <Codec title="Dolby Vision, dvhe" contentType={'video/mp4; codecs="dvhe.05.07"'} />
-            <Codec title="Dolby Vision, dvh1" contentType={'video/mp4; codecs="dvh1.05.07"'} />
-          </Grid>
-        </GridWrapper>
-        <GridWrapper>
-          <h2>AUDIO CODECS - audio/mp4</h2>
+          <h2>AUDIO - mp4</h2>
           <Grid>
             <Codec
               title="Advanced Audio Coding, AAC"
